b2ce86d72e5aa1718a38d594b42004ef9c8b76ff
Biswa-bob <56729624+Biswa-bob@users.noreply.github.com>
2021-06-26 17:17:04 +0530

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
 function Footer() {
-  const year = new Date();
-  const newYear = year.getFullYear();
   return (
     <footer>
-      <p>Copyright by ShapeAI @ {newYear}</p>
+      <p>Copyright by ShapeAI @ {new Date().getFullYear()}</p>
     </footer>
   );
 }
